9b5d6bef22eef76665a76173e384ab3b4ef9e125
cbuist8776 <cbuist8776@Chadwicks-MacBook-Pro.local>
2014-08-14 21:16:29 -0400

    Personal Expressions Title Given
    Created Title for Personal Expressions and title label for the
    script.js file for the assignment

--- Expressions_Personal/JS/script.js
+++ Expressions_Personal/JS/script.js
@@ -1,1 +1,22 @@
-alert("This is in the js file");
+/* Chadwick Buist
+ * WPF 1408 Section 1
+ * Personal Expressions Assignment
+ * 8-14-2014
+ */
+// Produce givens that there is a 20 gallon tank, the trip is 180 miles one way, and the price per gallon is $3.40.
+
+var tank = 20 + "gallons";
+
+var distance = 180*2 + "miles";
+
+var price = 3.40 + "dollars and cents";
+
+//Ask the driver how many times he or she filled up?
+
+var fillups = prompt(How many times did you fill up on the whole trip?);
+
+//Calculate the total gallons and multiply it by the price.
+
+var total = fillups*3.40;
+
+
