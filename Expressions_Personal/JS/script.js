8fa741b79a0af9317869cdf2620bd051ff3e4121
cbuist8776 <cbuist8776@Chadwicks-MacBook-Pro.local>
2014-08-15 19:31:34 -0400

    Total Times Driven variable added.
    Total Times Driven Variable added.

--- Expressions_Personal/JS/script.js
+++ Expressions_Personal/JS/script.js
@@ -3,12 +3,27 @@
  * Personal Expressions Assignment
  * 8-14-2014
  */
-//
+// Calculate the total miles for the trip. Calculate how many trips were made. Calculate the total cost of gas for the entire trip.
+
 var tank = 20;
 
-var distance = 180*2;
+var distance = 180;
 
 var price = 3.40;
+
+//Calculate the total miles for the trip.
+
+var totalTripDistance = 180*x
+
+//Ask for total miles driven by asking how many one way trips were made.
+
+var totalTimesDriven = prompt("How many one way trips were made?");
+
+console.log(totalTimesDriven)
+
+
+
+
 
 //Ask the driver how many times he or she filled up?
 
