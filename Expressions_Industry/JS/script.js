c8ece24a7249475785a881c38caaef1f67bc2bfe
cbuist8776 <cbuist8776@Chadwicks-MacBook-Pro.local>
2014-08-15 19:59:43 -0400

    variables set for Professional expressions
    Given variables set for professional expressions

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -4,20 +4,13 @@
  * Industry Specific Expression
  * 8-14-2014
  */
-//How many website clients to reach $1500 in profit at $100 per client to be calculated. How many to reach $2500 in profit? How many to reach $10,000 in profit?
+//How many website clients to reach $1500 in profit at $100 per client. How many clients at $120 per client? How many clients at $140 per client?
 
 //Figure out how many clients it would take to bank $1500 total.
 
-var client = 100 + "dollars";
+var client = 100 + "dollars"
 
-//Calculate how many clients he/she would need to reach %1500 in fees.
+var client2 = 120 + "dollars"
 
-var total = 1500/client
+var client3 = 140 + "dollars"
 
-//Print to console
-
-console.log(total);
-
-//Alert the user of the total.
-
-alert(total);
