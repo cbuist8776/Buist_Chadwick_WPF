55c07fcedba432a2fb5af544778623153c6c4629
cbuist8776 <cbuist8776@Chadwicks-MacBook-Pro.local>
2014-08-15 20:59:23 -0400

    edit made
    edit made

--- Expressions_Wacky/JS/script.js
+++ Expressions_Wacky/JS/script.js
@@ -20,7 +20,7 @@ console.log(twentyFive);
 
 //Alert the athlete for the number of strokes per 25 meters
 
-alert("At one stroke per meter you will take" + "" +  twentyFive  + "" + "strokes per 25 meters");
+alert("At one stroke per meter you will take" + "" +twentyFive+ "" + "strokes per 25 meters");
 
 //Calculate number of strokes per 50 meters.
 
