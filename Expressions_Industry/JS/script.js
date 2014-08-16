2f8014b407f48513465e1f656c9062c424f159bf
cbuist8776 <cbuist8776@Chadwicks-MacBook-Pro.local>
2014-08-15 20:06:04 -0400

    console.log and alert set up.
    console.log and alert set up for var oneHundred

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -8,15 +8,21 @@
 
 //Figure out how many clients it would take to bank $1500 total.
 
-var client = 100 + "dollars"
+var client = 100 + "dollars";
 
-var client2 = 120 + "dollars"
+var client2 = 120 + "dollars";
 
-var client3 = 140 + "dollars"
+var client3 = 140 + "dollars";
 
 //Calculate how many clients to reach $1500 at $100 per client.
 
-var oneHundred = 1500/100
+var oneHundred = 1500/100;
 
+//Write to var oneHundred to console
 
+console.log(oneHundred);
+
+//Set up Alert to user for var oneHundred
+
+alert(oneHundred)
 
