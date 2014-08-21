9d6b87c70c76d22c36554e62d0e912b5f430ef6a
cbuist8776 <cbuist8776@chadwicks-mbp.home>
2014-08-21 18:28:11 -0400

    Name prompt added to Cond. Personal project
    Name prompt added to Cond. Personal project.

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -5,3 +5,13 @@
  * 8-21-2014
  */
 
+//Calculate whether or not I should go out with a person based on where we live, how much we make, and if we like each other.
+
+//Ask the woman's name.
+
+var name = prompt("What is your name?")
+
+//Validate the prompt
+
+if(name === "") {name= prompt("Please enter your name!");}
+
