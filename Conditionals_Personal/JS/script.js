a4e2eadf2be8bef00ff4b548e4dfed7065bd369d
cbuist8776 <cbuist8776@chadwicks-mbp.home>
2014-08-21 18:33:32 -0400

    Where do you live prompt added.
    Where do you live prompt added.

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -9,9 +9,17 @@
 
 //Ask the woman's name.
 
-var name = prompt("What is your name?")
+var name = prompt("What is your name?");
 
 //Validate the prompt
 
 if(name === "") {name= prompt("Please enter your name!");}
+
+//Ask where the person lives and validate the prompt.
+
+var residence = prompt("Where do you live?");
+
+//Validate the prompt
+
+if (residence === ""){name= prompt("Please enter where you live!");}
 
