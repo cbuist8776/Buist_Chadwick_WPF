4f33e9e5df83b8a0920a3d071036392016462835
cbuist8776 <cbuist8776@chadwicks-mbp.home>
2014-08-28 16:22:15 -0400

    trying to squeeze variables into function
    trying to squeeze variables into function

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -6,31 +6,7 @@
  */
 //Calculate how many push-ups, sit ups, and squats I must do combined to  make either team A's standards or team B's standards.
 
-//Prompt user for the number of push-ups, sit ups, and squats completed and validate the prompt.
-
-var pushup = prompt("How many pushups did you complete?")
-
-//Validate the prompt
-
-while(pushup === ""){pushup = prompt ("Please enter the number of pushups completed!");}
-
-console.log(pushup)
-
-var situp= prompt ("How many situps did you complete?")
-
-//Validate the prompt
-
-while(situp === ""){situp = prompt ("Please enter the number of situps completed!")}
-
-console.log(situp)
-
-var squat= prompt("How many squats did you complete?")
-
-//Validate the prompt
-
-while(squat === ""){squat = prompt ("Please enter the number of squats you completed!")}
-
-console.log(squat)
+//Prompt user for the number of push-ups, sit ups, and squats completed and validate the prompt with a function.
 
 //Standard to make team A is average 150 or greater.  Standard to make team B is average 100 or greater.  Calculate which team I will be on after information is entered.
 
@@ -39,9 +15,27 @@ console.log(squat)
 function teamTotal(){
 	var pushup = prompt("How many pushups did you complete?")
 	
+	//Validate the prompt
+
+while(pushup === ""){pushup = prompt ("Please enter the number of pushups completed!");}
+
+console.log(pushup)
+	
 	var situp = prompt ("How many situps did you complete?")
 	
+	//Validate the prompt
+
+while(situp === ""){situp = prompt ("Please enter the number of situps completed!")}
+
+console.log(situp)
+	
 	var squat = prompt("How many squats did you complete?")
+	
+	//Validate the prompt
+
+while(squat === ""){squat = prompt ("Please enter the number of squats you completed!")}
+
+console.log(squat)
 	
 	var total= pushup * situp * squat
 	console.log(total);}
