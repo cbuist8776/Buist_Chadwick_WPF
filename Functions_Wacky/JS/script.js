7a2d4370e4ca372f72fc643b93a804709372be8f
cbuist8776 <cbuist8776@chadwicks-mbp.home>
2014-08-28 18:06:22 -0400

    wacky prompt set and validated.
    Wacky prompt set and validated.

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -5,3 +5,12 @@
  * 8-28-2014
  */
 
+//Calculate how many cars it will take to fill the parking spaces at the party this Labor Day Weekend.
+
+var cars = prompt("How many cars are you bringing to the party?")
+
+//Validate the prompt
+
+while(cars === ""){cars = prompt ("Please enter the number of cars you are bringing to the party);}
+
+console.log(cars)
