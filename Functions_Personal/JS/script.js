65bb9bd7151a111bed8a0f9a05d823e9976c39bb
cbuist8776 <cbuist8776@chadwicks-mbp.home>
2014-08-28 15:24:21 -0400

    variables and initial validations added.
    Variables and initial validations added.

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -4,3 +4,25 @@
  * Functions_Personal
  * 8-28-2014
  */
+//Calculate how many push-ups, sit ups, and squats I must do combined to  make either team A's standards or team B's standards.
+
+//Prompt user for the number of push-ups, sit ups, and squats completed and validate the prompt.
+
+var pushup = prompt("How many pushups did you complete?")
+
+//Validate the prompt
+
+if (pushup === ""){pushup = prompt ("Please enter the number of pushups completed!");}
+
+var situp= prompt ("How many situps did you complete?")
+
+//Validate the prompt
+
+if (situp === ""){situp = prompt ("Please enter the number of situps completed!")}
+
+var squat= prompt("How many squats did you complete?")
+
+//Validate the prompt
+
+if (squat === ""){squat = prompt ("Please enter the number of squats you completed!")}
+
