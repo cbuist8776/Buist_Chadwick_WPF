ff4c6a07dbf4130bf4e261fefe03bd453249326b
cbuist8776 <cbuist8776@chadwicks-mbp.home>
2014-08-28 18:32:31 -0400

    function edited.
    function edited

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -7,23 +7,29 @@
 
 //Calculate how many cars it will take to fill the parking spaces at the party this Labor Day Weekend.
 
-var cars = prompt("How many cars are you bringing to the party?")
+
 
 //Validate the prompt
 
-while(cars === ""){cars = prompt ("Please enter the number of cars you are bringing to the party);}
 
-console.log(cars)
 
 //Create a function to see how many cars it will take to fill up the 20 parking spaces before running out of space.
 
 function parking () {
-	var cars = 1 
-	var spaces = 20
+	
+	var cars = prompt("How many cars are you bringing to the party?")
+	
+	while(cars === ""){cars = prompt ("Please enter the number of cars you're bringing to the party!");}
+
+    console.log(cars);
+    
 	var capacity = spaces/cars
+	
 	console.log(capacity);}
 	
 	parking();
+	
+	
 
 
 
