a387d80fab54e32f260fa85008cef83ca82d1c43
cbuist8776 <cbuist8776@chadwicks-mbp.home>
2014-08-28 17:38:03 -0400

    variable entered, prompted, and validated.
    Variable for desktop PC’s entered, prompted, and validated.

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -4,3 +4,15 @@
  * Functions_Industry Project
  * 8-28-2014
  */
+
+//Calculate how many monitors are needed for each desktop PC ordered.
+
+var desktop = prompt("How many desktop PC's are you ordering?")
+
+//Validate the prompt:
+
+while(desktop === ""){desktop = prompt ("Please enter the number of desktop PC's you wish to purchase");}
+
+console.log(desktop)
+
+
