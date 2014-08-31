441e57f93cd2affac710278fd07191afcf83e312
cbuist8776 <cbuist8776@chadwicks-mbp.home>
2014-08-31 08:41:40 -0400

    Questions for Professor Garlic Added.
    Questions for professor garlic added

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -1,5 +1,5 @@
-/*
- * Chadwick Buist
+/*  
+ *Chadwick Buist
  * WPF 1408 Section 1
  * Functions_Wacky Project
  * 8-28-2014
@@ -34,6 +34,17 @@ function parking (spaces, cars) {
 	var totalParking = parking(20, 40, 100);
 	
 	console.log (totalParking);
+	
+	
+	//Questions for Professor Garlic
+	// I know this function is very simple and before I make it further complex I'm getting an error in the console on line one that states "Unexpected end of input".
+	//I'm not sure what that error is pointing to and I'm having a hard time correcting it to run the function.
+	//Could you help me in that regard and then let me know if the code so far for this function, return value, call, etc are correct or not?
+	//Thank you once more for all your help as I do greatly appreciate it.
+	//Sincerely,
+	//Chad Buist
+	
+	
 	
 	
 	
